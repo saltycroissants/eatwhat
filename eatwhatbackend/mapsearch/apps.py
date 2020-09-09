166cccccd73b04d55098a88dbe4eda22a8bcdde9
eunji lee <eunjilee@eunjiui-MacBook-Pro-93.local>
2020-09-09 13:32:01 +0900

    added api

---
+++ eatwhatbackend/mapsearch/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MapsearchConfig(AppConfig):
+    name = 'mapsearch'
---
+++ eatwhatbackend/mapsearch/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+import os
+import sys
+import json
+import requests #pip3 install requests
+# Create your views here.
+
+def index(request):
+    url = "https://openapi.naver.com/v1/search/local.json"
+    param = {
+        "query" : "노은동 고기",
+        "display" : 5
+    }
+    nheaders ={
+        "X-Naver-Client-Id" : "hBwX2RgYzXT21cJDCpTR",
+        "X-Naver-Client-Secret" : "jSx89D1yl0"
+    }
+    res = requests.get(url,headers =nheaders, params =param)
+    #log.debug(res.status_code)
+    return JsonResponse(res.json())
+    
